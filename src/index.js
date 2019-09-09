20ded204fb7d4d15bf02543b0d6dd83c35c0833c
xj <908634674@qq.com>
2019-09-09 14:10:14 +0800

    更新

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import {
+  dateToStr,
+  dateDiff,
+  dateAdd,
+  isLeapYear,
+  strToDate,
+  strFormatToDate,
+  dateToLong,
+  longToDate,
+  isDate,
+  getMaxDay,
+  maxDayOfDate,
+  datePart
+} from './date'
+import nav from './nav'
+import verify from './verify'
+import { getQuery, getUrlParam } from './query'
+import { set as setGlobalData, get as getGlobalData } from './global-data'
+import { gcjEncrypt, gcjDecrypt, gcjDecryptExact, bdEncrypt, bdDecrypt, distance } from './gps'
+import { randomString, phoneHiding } from './string'
+import md5 from './md5'
+import { base64Encode, base64Decode } from './base64'
+
+export {
+  dateToStr,
+  dateDiff,
+  dateAdd,
+  isLeapYear,
+  strToDate,
+  strFormatToDate,
+  dateToLong,
+  longToDate,
+  isDate,
+  getMaxDay,
+  maxDayOfDate,
+  datePart,
+  nav,
+  verify,
+  getQuery,
+  getUrlParam,
+  setGlobalData,
+  getGlobalData,
+  gcjEncrypt, 
+  gcjDecrypt, 
+  gcjDecryptExact, 
+  bdEncrypt, 
+  bdDecrypt, 
+  distance,
+  randomString,
+  phoneHiding,
+  md5,
+  base64Encode,
+  base64Decode
+}
